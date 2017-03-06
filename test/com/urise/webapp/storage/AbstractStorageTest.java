60dfb54170fad4196499072a85314f1e86b95f7b
YourFullName <diversi93@mail.ru>
2017-03-06 04:55:52 +0300

    HW6

--- test/com/urise/webapp/storage/AbstractStorageTest.java
+++ test/com/urise/webapp/storage/AbstractStorageTest.java
@@ -17,16 +17,20 @@ import static org.junit.Assert.*;
 public class AbstractStorageTest {
     private Storage storage;
     private static final String UUID_1 = "uuid1";
-    private static final Resume RESUME_1 = new Resume(UUID_1);
+    private static final String NAME_1 = "name1";
+    private static final Resume RESUME_1 = new Resume(UUID_1, NAME_1);
 
     private static final String UUID_2 = "uuid2";
-    private static final Resume RESUME_2 = new Resume(UUID_2);
+    private static final String NAME_2 = "name2";
+    private static final Resume RESUME_2 = new Resume(UUID_2, NAME_2);
 
     private static final String UUID_3 = "uuid3";
-    private static final Resume RESUME_3 = new Resume(UUID_3);
+    private static final String NAME_3 = "name3";
+    private static final Resume RESUME_3 = new Resume(UUID_3, NAME_3);
 
     private static final String UUID_4 = "uuid4";
-    private static final Resume RESUME_4 = new Resume(UUID_4);
+    private static final String NAME_4 = "name4";
+    private static final Resume RESUME_4 = new Resume(UUID_4, NAME_4);
 
     protected AbstractStorageTest(Storage storage) {
         this.storage = storage;
